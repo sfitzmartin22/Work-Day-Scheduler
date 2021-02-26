416040c89d8436504f544bd14aee7378498ed808
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-02-26 15:43:46 -0500

    working through saving info based on the click, I'm using  to traverse through the immediate div of the save button and save the time and description of the task in an array.  I will remove console logs once i fix the bugs that I currently have when i'm trying to store the array in the local storage

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
+//Reload page //
+$(document).ready (function() {
+
+// define objects to store //
+var schedule = [];
+
+// when clicking a save button i run through the local div to find time and schedule details //
+$(".saveBtn").on("click", function() {
+   
+    var description = $(this).siblings(".description").val();
+    console.log(description)
+    var time = $(this).parent().attr("id");
+    console.log(time);
+   
+
+   // push into schedule //
+    schedule.push({time: time, description: description});
+   console.log(schedule);
+   // store objects in local storage //
+    var storeTasks = function(){
+    localStorage.setItem("schedule",JSON.stringify(schedule));
+}
+   
+}
+);
+
+})
+    
 // set date at top of the page //
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+// var addSchedule = function()
