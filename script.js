8abc95c6175f0696e23bc7349df37369f4440ce0
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-02-26 17:06:31 -0500

    unfinished for statement

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-//Reload page //
-$(document).ready (function() {
-
 // define array that the objects will be defined within //
 var schedule = [];
 
+//Reload page //
+$(document).ready (function() {
+
 // when clicking a save button i run through the local div to find time and schedule details //
 $(".saveBtn").on("click", function() {
-   
+
     var description = $(this).siblings(".description").val();
-    console.log(description)
     var time = $(this).parent().attr("id");
 
-   // push into schedule //
-   schedule.push({time: time, description: description});
+    // push into schedule //
+    schedule.push({description: description, time: time});
    
-   // store objects in local storage //
-   localStorage.setItem("schedule",JSON.stringify(schedule));
+    // store objects in local storage //
+    localStorage.setItem("schedule",JSON.stringify(schedule));
 }
 );
 })
@@ -23,5 +22,15 @@ $(".saveBtn").on("click", function() {
 // set date at top of the page //
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+// get local storage parse it //
+function updateSchedule () {
+var getSchedule = JSON.parse(localStorage.getItem("schedule"));
 
-// var addSchedule = function()
+//for statment to apply it to the correct area on the webpage//
+for(var i = 0; i < schedule.length; i++) {
+    var userInput = schedule[i].description;
+    $("#" + schedule[i].time).children(".description").text(userInput);
+}
+}
+
+updateSchedule();
