526383f6c095bebe31da709547419fda65315201
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-02-28 03:21:32 -0500

    fixed the bug in my background color function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // define array that the objects will be defined within //
 var schedule = [];
+// set date at top of the page //
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
 //Reload page //
@@ -34,38 +36,23 @@ $(".saveBtn").on("click", function() {
     
 }
 );
-})
-    
-// set date at top of the page //
-$("#currentDay").text(moment().format("dddd, MMMM Do"));
-
-// get local storage parse it //
-function updateSchedule () {
-var getSchedule = JSON.parse(localStorage.getItem("schedule"));
-
-//for statment to apply it to the correct area on the webpage//
-for(var i = 0; i < schedule.length; i++) {
-    var userInput = schedule[i].description;
-    $("#" + schedule[i].time).children(".description").text(userInput);
-}
-}
-
-updateSchedule();
-
-
 
 var hourAudit =function(){
     var currentHour = moment().hour() 
-
     for(var i=8; i<18; i++){
         var taskArea = $("#hour-"+i)  
         if(currentHour>i){
             $(taskArea).addClass("past");
-        } else if (currentHour === i){
+        } else if (currentHour == i){
             $(taskArea).addClass("present");
         }else{
-            $(taskArea).addClass("future")
+            $(taskArea).addClass("future");
         }
     }
 }
+
+hourAudit();
+
+})
+    
 
