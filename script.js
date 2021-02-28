9810145b20e9cb99091badadb5a3f0e75df20232
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-02-27 21:23:37 -0500

    displaying local storage on the page.  It works when i reload page but when i save a task again after reload, it writes over my local storage and i can't figure out how to adress this sitation.  Also began to try and figure out my for statement that colors the time-blocks

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // define array that the objects will be defined within //
 var schedule = [];
 
+
 //Reload page //
 $(document).ready (function() {
+
+
+    // get local storage parse it //
+    function updateSchedule () {
+    var getSchedule = JSON.parse(localStorage.getItem("schedule"));
+    
+    //for statment to apply it to the correct area on the webpage//
+    for(var i = 0; i < getSchedule.length; i++) {
+        var userInput = getSchedule[i].description;
+        $("#" + getSchedule[i].time).children(".description").text(userInput);
+    }
+    }
+
+    updateSchedule();
 
 // when clicking a save button i run through the local div to find time and schedule details //
 $(".saveBtn").on("click", function() {
@@ -15,6 +30,8 @@ $(".saveBtn").on("click", function() {
    
     // store objects in local storage //
     localStorage.setItem("schedule",JSON.stringify(schedule));
+
+    
 }
 );
 })
@@ -34,3 +51,21 @@ for(var i = 0; i < schedule.length; i++) {
 }
 
 updateSchedule();
+
+
+
+var hourAudit =function(){
+    var currentHour = moment().hour() 
+
+    for(var i=8; i<18; i++){
+        var taskArea = $("#hour-"+i)  
+        if(currentHour>i){
+            $(taskArea).addClass("past");
+        } else if (currentHour === i){
+            $(taskArea).addClass("present");
+        }else{
+            $(taskArea).addClass("future")
+        }
+    }
+}
+
